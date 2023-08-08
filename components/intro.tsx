4d91645f4c557c02efca4fa2eab0542776f1da3d
Yang Zhang <piaoling2118@gmail.com>
2023-08-08 14:21:22 -0400

    Header and Intro

---
+++ components/intro.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import Link from 'next/link';
+import React from 'react'
+import { BsArrowRight, BsLinkedin} from "react-icons/bs"
+import { FaGithubSquare } from 'react-icons/fa';
+import {HiDownload} from "react-icons/hi"
+
+export default function Intro() {
+  return (
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
+        <div className='flex items-center justify-center'>
+            <div className='relative'>
+                <motion.div
+                    initial ={{opacity:0, scale:0}}
+                    animate={{opacity:1, scale:1}}
+                    transition={{
+                        type:"tween",
+                        duration:0.2,
+                    }}>
+                <Image 
+                src="http://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100" 
+                alt="Recardo portrait" 
+                width="192"
+                height="192"
+                quality="95"
+                priority={true}
+                className='h-24 w-24 rounded-full object-cover shadow-x1 border-[0.35rem] border-white'
+                />
+                </motion.div>
+                
+                <motion.span className='absolute bottom-0 right-0 text-4*1'
+                    initial={{opacity:0, scale:0 }}
+                    animate={{opacity:1, scale:1 }}
+                    transition={{
+                        type:'spring',
+                        stiffness:150,
+                        delay:0.2,
+                        duration:0.7,
+                    }}>
+                    🤪
+                </motion.span>
+            </div>
+        </div>
+        <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+            initial={{opacity:0, y:100}}
+            animate={{opacity:1, y:0}}
+            >
+            <span className='font-bold'>Hello, I'm Yang.</span> I'm a {" "}
+            <span className='font-bold'>Computer Engineering student</span> at{" "}
+            <span className='font-bold'>Concordia University.</span>I has learnt to build sites & Apps.{" "} This website use
+            <span className='italic'>Next.js, TypeScipt, Tailwind CSS.</span>
+        </motion.p>
+
+        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+            initial={{opacity:0,y:100}}
+            animate={{opacity:1,y:0}}
+            transition ={{
+                delay:0.1,
+            }}
+            >
+            <Link href="#contact" 
+            className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+                 Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+            </Link>
+            <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10' 
+            href="/CV.pdf" download>
+                Download CV{" "}<HiDownload className="opacity-60 group-hover:translate-y-1 transition"/>
+            </a>
+
+            <a className='bg-white p-4 text-gray-600 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'
+                href="http://linkedin.com" target="_blank">
+                <BsLinkedin />
+            </a>
+
+            <a className='bg-white p-4 text-gray-600 flex items-center gap-2 rounded-full text-[1.3rem] focus:scale-110 hover:scale-110  hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'
+                href="http://github.com" target="_blank">
+                <FaGithubSquare/>
+            </a>
+        </motion.div>
+    </section>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol:"http",
+                hostname:"images.unsplash.com",
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig
